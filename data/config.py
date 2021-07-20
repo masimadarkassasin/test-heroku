ddaf344ba5e449e03488af554a445a7bb1c5b517
masimadarkassasin <87667380+masimadarkassasin@users.noreply.github.com>
2021-07-21 00:58:58 +0300

    Delete config.py

--- data/config.py
+++
@@ -1,8 +0,0 @@
-from environs import Env
-
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
-env = Env()
-env.read_env()
-
-BOT_TOKEN = "1722059977:AAEpKG7MLK4nYSjsqDgM6IBfPhYBo7S2RPc"  # Забираем значение типа str
-ADMINS = 997430015 # Тут у нас будет список из админов
